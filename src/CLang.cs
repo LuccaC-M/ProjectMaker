9705aebf8e496a92283df36d795c5e142cbac262
LuccaC.M <git.yfm@slmail.me>
2023-04-16 19:24:19 -0400

    Added CreateHeaderFile

--- src/CLang.cs
+++ src/CLang.cs
@@ -5,7 +5,7 @@ namespace C {
     class CLang {
         public static bool CreateFiles(string fileName) {
             string filePath = Directory.GetCurrentDirectory() + '/';
-            return CreateSourceFile(fileName, filePath);
+            return CreateSourceFile(fileName, filePath) && CreateHeaderFile(fileName, filePath);
         }
 
         static bool CreateSourceFile(string fileName, string filePath) {
@@ -14,5 +14,14 @@ namespace C {
             File.WriteAllText(filePath, $"#include \"{fileName + ".h"}\"");
             return File.Exists(filePath);
         }
+
+        static bool CreateHeaderFile(string fileName, string filePath) {
+            filePath += fileName + ".h";
+            fileName = fileName.ToUpper();
+            // Create & Write to file     #include "the_name_of_the_file.h"
+            File.WriteAllText(filePath, $"#ifndef {fileName + "_H"} \n#define {fileName + "_H"} \n #endif //{fileName + "_H"}");
+            return File.Exists(filePath);
+        }
+
     }
 }
