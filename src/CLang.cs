9f346130781c51e15725516b31bb78d4a5b6c64c
LuccaC.M <git.yfm@slmail.me>
2023-04-16 19:28:08 -0400

    Fixed space before #endif

--- src/CLang.cs
+++ src/CLang.cs
@@ -19,7 +19,7 @@ namespace C {
             filePath += fileName + ".h";
             fileName = fileName.ToUpper();
             // Create & Write to file     #include "the_name_of_the_file.h"
-            File.WriteAllText(filePath, $"#ifndef {fileName + "_H"} \n#define {fileName + "_H"} \n #endif //{fileName + "_H"}");
+            File.WriteAllText(filePath, $"#ifndef {fileName + "_H"} \n#define {fileName + "_H"} \n#endif //{fileName + "_H"}");
             return File.Exists(filePath);
         }
 
