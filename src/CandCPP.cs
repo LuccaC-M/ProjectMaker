e0fc0f1992a396e39c3219fb16854e0d466e3108
LuccaC.M <git.yfm@slmail.me>
2023-04-17 16:57:18 -0400

    Added CPP Files namespace

--- src/CandCPP.cs
+++ src/CandCPP.cs
@@ -25,3 +25,28 @@ namespace Cfiles {
 
     }
 }
+
+namespace CPPfiles {
+    class CPP {
+        public static bool CreateFiles(string fileName, short suffix) {
+            string filePath = Directory.GetCurrentDirectory() + '/';
+            return CreateSourceFile(fileName, filePath, suffix) && CreateHeaderFile(fileName, filePath, suffix);
+        }
+
+        static bool CreateSourceFile(string fileName, string filePath, short suffix) {
+            filePath += fileName + (suffix == 1 ? ".cpp" : suffix == 2 ? ".cc" : ".cxx");
+            // Create the file & add the header file
+            File.WriteAllText(filePath, $"#include <{fileName + ".h"}>");
+            return File.Exists(filePath);
+        }
+
+        static bool CreateHeaderFile(string fileName, string filePath, short suffix) {
+            filePath += fileName + (suffix == 1 ? ".hpp" : suffix == 2 ? ".hh" : ".hxx");
+            fileName = fileName.ToUpper();
+            // Create the file & add include guards
+            File.WriteAllText(filePath, $"#ifndef {fileName + "_H"} \n#define {fileName + "_H"} \n#endif //{fileName + "_H"}");
+            return File.Exists(filePath);
+        }
+
+    }
+}
