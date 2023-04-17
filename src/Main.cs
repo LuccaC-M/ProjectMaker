922d616a690ddf501e9f978848373004168d3305
LuccaC.M <git.yfm@slmail.me>
2023-04-17 18:32:24 -0400

    Removed unecessary main namespace

--- src/Main.cs
+++ src/Main.cs
@@ -2,41 +2,39 @@ using System;
 using Cfiles;
 using CPPfiles;
 
-namespace Principal {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Choose Project Type:");
-            Console.Write("1. C \n2. C++ \nInput a Number: ");
-            int choice = Int32.Parse(Console.ReadLine());
-            
-            string fileName;
-            bool success;
-            switch(choice) {
-                case 1:
-                    Console.Write("Input file name (without suffix): ");
-                    fileName = Console.ReadLine();
-                    success = CLang.CreateFiles(fileName);
-                    if (success) {
-                        Console.WriteLine("C Files have been created!");
-                    } else {
-                        Console.WriteLine("C File(s) couldn't be created");
-                    }
-                    break;
-                case 2:
-                    Console.Write("Input file name (without suffix): ");
-                    fileName = Console.ReadLine();
-                    Console.WriteLine("Choose suffix:");
-                    Console.Write("1. .cpp \n2. .cc \n3. .cxx \n> ");
-                    short suffix = short.Parse(Console.ReadLine());
-                    success = CPP.CreateFiles(fileName, suffix);
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Choose Project Type:");
+        Console.Write("1. C \n2. C++ \nInput a Number: ");
+        int choice = Int32.Parse(Console.ReadLine());
+        
+        string fileName;
+        bool success;
+        switch(choice) {
+            case 1:
+                Console.Write("Input file name (without suffix): ");
+                fileName = Console.ReadLine();
+                success = CLang.CreateFiles(fileName);
+                if (success) {
+                    Console.WriteLine("C Files have been created!");
+                } else {
+                    Console.WriteLine("C File(s) couldn't be created");
+                }
+                break;
+            case 2:
+                Console.Write("Input file name (without suffix): ");
+                fileName = Console.ReadLine();
+                Console.WriteLine("Choose suffix:");
+                Console.Write("1. .cpp \n2. .cc \n3. .cxx \n> ");
+                short suffix = short.Parse(Console.ReadLine());
+                success = CPP.CreateFiles(fileName, suffix);
 
-                    if (success) {
-                        Console.WriteLine("C++ Files have been created!");
-                    } else {
-                        Console.WriteLine("C++ File(s) couldn't be created");
-                    }
-                    break;
-            }
+                if (success) {
+                    Console.WriteLine("C++ Files have been created!");
+                } else {
+                    Console.WriteLine("C++ File(s) couldn't be created");
+                }
+                break;
         }
     }
 }
