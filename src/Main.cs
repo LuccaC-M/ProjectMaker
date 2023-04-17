7da744ef7345e7bb3336474fde4868ceae3b82e0
LuccaC.M <git.yfm@slmail.me>
2023-04-17 16:59:34 -0400

    Changed Added CPP case

--- src/Main.cs
+++ src/Main.cs
@@ -1,5 +1,6 @@
 using System;
-using C;
+using Cfiles;
+using CPPfiles;
 
 namespace Principal {
     class Program {
@@ -8,19 +9,32 @@ namespace Principal {
             Console.Write("1. C \n2. C++ \nInput a Number: ");
             int choice = Int32.Parse(Console.ReadLine());
             
+            string fileName;
+            bool success;
             switch(choice) {
                 case 1:
                     Console.Write("Input file name (without suffix): ");
-                    string fileName = Console.ReadLine();
-                    bool success = CLang.CreateFiles(fileName);
+                    fileName = Console.ReadLine();
+                    success = CLang.CreateFiles(fileName);
                     if (success) {
-                        Console.WriteLine("C File has been created!");
+                        Console.WriteLine("C Files have been created!");
                     } else {
                         Console.WriteLine("C File(s) couldn't be created");
                     }
                     break;
                 case 2:
-                    Console.WriteLine("C++ File has been created!");
+                    Console.Write("Input file name (without suffix): ");
+                    fileName = Console.ReadLine();
+                    Console.WriteLine("Choose suffix:");
+                    Console.Write("1. .cpp \n2. .cc \n3. .cxx \n> ");
+                    short suffix = short.Parse(Console.ReadLine());
+                    success = CPP.CreateFiles(fileName, suffix);
+
+                    if (success) {
+                        Console.WriteLine("C++ Files have been created!");
+                    } else {
+                        Console.WriteLine("C++ File(s) couldn't be created");
+                    }
                     break;
             }
         }
