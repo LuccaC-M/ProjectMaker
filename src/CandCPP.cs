95963a9ec459881908e6c252910e1c614d14c1c3
LuccaC.M <git.yfm@slmail.me>
2023-04-22 13:59:17 -0400

    Added CreateMainFile & Added hearderGuard variable to CreateFile

--- src/CandCPP.cs
+++ src/CandCPP.cs
@@ -5,6 +5,7 @@ namespace Cfiles {
     class CLang {
 
         public static void CreateFiles(string[] fileNames, string filePath) {
+            CreateMainFile(filePath);
             foreach (string fileName in fileNames) {
                 CreateSourceFile(fileName, filePath);
                 CreateHeaderFile(fileName, filePath);
@@ -19,17 +20,22 @@ namespace Cfiles {
 
         static void CreateHeaderFile(string fileName, string filePath) {
             filePath += fileName + ".h";
-            fileName = fileName.ToUpper();
+            headerGuard = fileName.ToUpper() + "_H";
             // Create the file & add include guards
-            File.WriteAllText(filePath, $"#ifndef {fileName + "_H"} \n#define {fileName + "_H"} \n#endif //{fileName + "_H"}");
+            File.WriteAllText(filePath, $"#ifndef {headerGuard} \n#define {headerGuard} \n#endif //{headerGuard}");
         }
 
+        static void CreateMainFile(string filePath) {
+            filePath += "main.c";
+            File.WriteAllText(filePath, "int main(int argc, char *argv[]) {\n}");
+        }
     }
 }
 
 namespace CPPfiles {
     class CPP {
         public static void CreateFiles(string[] fileNames, string filePath, short suffix) {
+            CreateMainFile(filePath, suffix);
             foreach (string fileName in fileNames) {
                 CreateSourceFile(fileName, filePath, suffix);
                 CreateHeaderFile(fileName, filePath, suffix);
@@ -39,14 +45,21 @@ namespace CPPfiles {
         static void CreateSourceFile(string fileName, string filePath, short suffix) {
             filePath += fileName + (suffix == 1 ? ".cpp" : suffix == 2 ? ".cc" : ".cxx");
             // Create the file & add the header file
-            File.WriteAllText(filePath, $"#include <{fileName + ".h"}>");
+            File.WriteAllText(filePath, $"#include "{fileName + ".h"}"");
         }
 
         static void CreateHeaderFile(string fileName, string filePath, short suffix) {
-            filePath += fileName + (suffix == 1 ? ".hpp" : suffix == 2 ? ".hh" : ".hxx");
-            fileName = fileName.ToUpper();
+            string headerSuffix = (suffix == 1 ? "hpp" : suffix == 2 ? "hh" : "hxx");
+            filePath += fileName + "." + headerSuffix;
+            headerGuard = fileName.ToUpper() + "_" + headerSuffix.ToUpper();
             // Create the file & add include guards
-            File.WriteAllText(filePath, $"#ifndef {fileName + "_H"} \n#define {fileName + "_H"} \n#endif //{fileName + "_H"}");
+            File.WriteAllText(filePath, $"#ifndef {headerGuard} \n#define {headerGuard} \n#endif //{headerGuard}");
+        }
+
+        static void CreateMainFile(string filePath, short suffix) {
+            filePath += "main" + (suffix == 1 ? ".cpp" : suffix == 2 ? ".cc" : ".cxx");
+            // Create & write main function template
+            File.WriteAllText(filePath, "int main(int argc, char **argv) {\n}");
         }
 
     }
