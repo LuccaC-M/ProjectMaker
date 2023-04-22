e2566fb9cbb5817a68decf03f7dc5dfbe0adf20f
LuccaC.M <git.yfm@slmail.me>
2023-04-22 13:41:44 -0400

    Removed unnesesary return values from functions

--- src/CandCPP.cs
+++ src/CandCPP.cs
@@ -11,19 +11,17 @@ namespace Cfiles {
             }
         }
 
-        static bool CreateSourceFile(string fileName, string filePath) {
+        static void CreateSourceFile(string fileName, string filePath) {
             filePath += fileName + ".c";
             // Create & Write to file     #include "the_name_of_the_file.h"
             File.WriteAllText(filePath, $"#include \"{fileName + ".h"}\"");
-            return File.Exists(filePath);
         }
 
-        static bool CreateHeaderFile(string fileName, string filePath) {
+        static void CreateHeaderFile(string fileName, string filePath) {
             filePath += fileName + ".h";
             fileName = fileName.ToUpper();
             // Create the file & add include guards
             File.WriteAllText(filePath, $"#ifndef {fileName + "_H"} \n#define {fileName + "_H"} \n#endif //{fileName + "_H"}");
-            return File.Exists(filePath);
         }
 
     }
@@ -38,19 +36,17 @@ namespace CPPfiles {
             }
         }
 
-        static bool CreateSourceFile(string fileName, string filePath, short suffix) {
+        static void CreateSourceFile(string fileName, string filePath, short suffix) {
             filePath += fileName + (suffix == 1 ? ".cpp" : suffix == 2 ? ".cc" : ".cxx");
             // Create the file & add the header file
             File.WriteAllText(filePath, $"#include <{fileName + ".h"}>");
-            return File.Exists(filePath);
         }
 
-        static bool CreateHeaderFile(string fileName, string filePath, short suffix) {
+        static void CreateHeaderFile(string fileName, string filePath, short suffix) {
             filePath += fileName + (suffix == 1 ? ".hpp" : suffix == 2 ? ".hh" : ".hxx");
             fileName = fileName.ToUpper();
             // Create the file & add include guards
             File.WriteAllText(filePath, $"#ifndef {fileName + "_H"} \n#define {fileName + "_H"} \n#endif //{fileName + "_H"}");
-            return File.Exists(filePath);
         }
 
     }
