64186c236bbf631c37a14702b85ebb8cd23e1611
LuccaC.M <git.yfm@slmail.me>
2023-04-26 17:45:53 -0400

    Added CreateDirectory

--- src/Main.cs
+++ src/Main.cs
@@ -1,13 +1,14 @@
 using System;
 using CandCPP;
-using FileUtilities;
+using SystemDirectoryUtils;
 
 class Program {
     static void Main(string[] args) {
         Console.WriteLine("Choose Project Type:");
         Console.Write("1. C \n2. C++ \nInput a Number: ");
         int choice = Int32.Parse(Console.ReadLine());
-        
+        Console.Write("Input project name: ");
+        string projectName = Console.ReadLine();
         string fileName;
         bool success;
         switch(choice) {
@@ -20,7 +21,8 @@ class Program {
                 Console.WriteLine("Choose suffix:");
                 Console.Write("1. .cpp \n2. .cc \n3. .cxx \n> ");
                 short suffix = short.Parse(Console.ReadLine());
-                CPP.CreateFiles(fileNames, System.IO.Directory.GetCurrentDirectory() + '/', suffix);
+                System.IO.Directory.CreateDirectory(System.IO.Directory.GetCurrentDirectory() + '/' + projectName);
+                CPP.CreateFiles(fileNames, System.IO.Directory.GetCurrentDirectory() + '/' + projectName, suffix);
                 break;
         }
     }
