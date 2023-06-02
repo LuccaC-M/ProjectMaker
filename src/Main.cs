b62c17b70177e7cc5ef8db4103787fb7ddedc3f0
LuccaC.M <git.yfm@slmail.me>
2023-06-02 07:17:39 -0400

    Fixed file-directory bug

--- src/Main.cs
+++ src/Main.cs
@@ -4,25 +4,32 @@ using SystemDirectoryUtils;
 
 class Program {
     static void Main(string[] args) {
+        /**Constants**/
+        string currentDirectory = System.IO.Directory.GetCurrentDirectory() + '/';
+
+        /**Getting the type of projects**/
         Console.WriteLine("Choose Project Type:");
         Console.Write("1. C \n2. C++ \nInput a Number: ");
-        int choice = Int32.Parse(Console.ReadLine());
+        int choice = Int32.Parse(Console.ReadLine()); // recording the input
+
+        /**Obtaining Project name**/
         Console.Write("Input project name: ");
         string projectName = Console.ReadLine();
-        string[] fileNames;
+        
+        /**Obtaing fileNames array**/
+        string[] fileNames /*Array with all the file names*/ = FileUtils.ObtainFileNamesArray();
+        
         switch(choice) {
             case 1:
-                fileNames = FileUtils.ObtainFileNamesArray();
-                System.IO.Directory.CreateDirectory(System.IO.Directory.GetCurrentDirectory() + '/' + projectName);
-                CLang.CreateFiles(fileNames, System.IO.Directory.GetCurrentDirectory() + $"/{projectName}");
+                System.IO.Directory.CreateDirectory(currentDirectory + projectName);
+                CLang.CreateFiles(fileNames, currentDirectory + $"/{projectName}/");
                 break;
             case 2:
-                fileNames = FileUtils.ObtainFileNamesArray();
                 Console.WriteLine("Choose suffix:");
                 Console.Write("1. .cpp \n2. .cc \n3. .cxx \n> ");
                 short suffix = short.Parse(Console.ReadLine());
-                System.IO.Directory.CreateDirectory(System.IO.Directory.GetCurrentDirectory() + '/' + projectName);
-                CPP.CreateFiles(fileNames, System.IO.Directory.GetCurrentDirectory() + '/' + projectName, suffix);
+                System.IO.Directory.CreateDirectory(currentDirectory + projectName);
+                CPP.CreateFiles(fileNames, System.IO.Directory.GetCurrentDirectory() + $"/{projectName}/", suffix);
                 break;
         }
     }
