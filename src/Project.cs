8fbe1731239dd2d9d5866318ebb690f94a6e3cb2
LuccaC.M <git.yfm@slmail.me>
2023-06-22 18:58:42 -0400

    Added comments to Project.cs

--- src/Project.cs
+++ src/Project.cs
@@ -1,11 +1,15 @@
 
 namespace Project {
     class abstract Project {
+        /* Main function it starts the projects by creating the main project directory first, then
+         * it creates the src folder inside, and calls the CreateFiles function. */
         public static StartProject(string[] fileNames, string currentDirectory, string projectName) {
             System.IO.Directory.CreateDirectory(currentDirectory + $"/{projectName}/");
             System.IO.Directory.CreateDirectory(currentDirectory + $"/{projectName}/" + "src");
             this.CreateFiles(filenames, currentDirectory + $"/{projectName}/");
         }
+        
+        // Function should be inplemented by every child and should create the nessesary files for the project.
         public abstract static CreateFiles();
     }
 }
