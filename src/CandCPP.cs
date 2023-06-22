9ff3d315f8e3391d95564ad00b2ed7be3b1b4926
LuccaC.M <git.yfm@slmail.me>
2023-06-22 18:55:07 -0400

    Added comments to CandCPP.cs

--- src/CandCPP.cs
+++ src/CandCPP.cs
@@ -4,7 +4,7 @@ using Project;
 
 namespace CandCPP {
     class CLang : Project {
-
+        // Changed the Start Project function to add type, a variable that decides if the Project is in C or C++
         public override static StartProject(string[] fileNames, string currentDirectory, string projectName, short type) {
             System.IO.Directory.CreateDirectory(currentDirectory + $"/{projectName}/");
             System.IO.Directory.CreateDirectory(currentDirectory + $"/{projectName}/" + "src");
@@ -12,20 +12,24 @@ namespace CandCPP {
         }
 
         public override static void CreateFiles(string[] fileNames, string filePath, short type) {
+            // Loop over the files and create a source and header files per file name
             foreach (string fileName in fileNames) {
                 CreateSourceFile(fileName, filePath + "src/", type);
                 CreateHeaderFile(fileName, filePath + "src/");
             }
+            // Create the main file
             CreateMainFile(filePath, type);
         }
 
         static void CreateSourceFile(string fileName, string filePath, short type) {
+            // Add the file name to the filePath and append the suffix depending on type
             filePath += fileName + (type == 1 ? ".c" + ".cc");
             // Create & Write to file     #include "the_name_of_the_file.h"
             File.WriteAllText(filePath, $"#include \"{fileName + ".h"}\"");
         }
 
         static void CreateHeaderFile(string fileName, string filePath) {
+            // Add the file name to the file path
             filePath += fileName + ".h";
             string headerGuard = fileName.ToUpper() + "_H";
             // Create the file & add include guards
@@ -33,7 +37,9 @@ namespace CandCPP {
         }
 
         static void CreateMainFile(string filePath, short type) {
-            filePath += type == 1 ? "main.c" : "main.cc";
+            // Add main to the file path and append the suffix depending on type
+            filePath += (type == 1 ? "main.c" : "main.cc");
+            // Create the main file and write the main function
             File.WriteAllText(filePath, "int main(int argc, char *argv[]) {\n}");
         }
     }
